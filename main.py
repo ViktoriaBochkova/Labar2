c8563cb68d40dacf5d4f11db9f2a8da6793a9171
ViktoriaBochkova <vicka.bo4ckova@yandex.ru>
2022-09-13 12:36:05 +0300

    second commit

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ weight=120
 step_number=1000
 active_time=60
 """
-height=float(input("Введите ваш рост"))
-weight=float(input("Введите ваш вес"))
-step_number=float(input("Введите кол-во шагов"))
-active_time=float(input("Введите время активности"))
+height=float(input("Введите ваш рост "))
+weight=float(input("Введите ваш вес "))
+step_number=float(input("Введите кол-во шагов "))
+active_time=float(input("Введите время активности "))
 
 step=height/4+0.37
 dist=step*step_number
@@ -15,4 +15,4 @@ dist=step*step_number
 speed=dist/active_time
 
 calor=0.035*weight+(speed**2/height)*0.029*weight
-print('пройденная дистанция',dist,'сожженных калории',calor)
+print('пройденная дистанция',dist/1000,'км,','сожженных калории',calor)
