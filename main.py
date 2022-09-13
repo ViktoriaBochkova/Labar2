860c317dd3bc2a01d656689ca0b8b937f12a8e2a
ViktoriaBochkova <vicka.bo4ckova@yandex.ru>
2022-09-13 19:36:56 +0300

    third commit

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ weight=120
 step_number=1000
 active_time=60
 """
+
 height=float(input("Введите ваш рост "))
 weight=float(input("Введите ваш вес "))
 step_number=float(input("Введите кол-во шагов "))
@@ -16,3 +17,10 @@ speed=dist/active_time
 
 calor=0.035*weight+(speed**2/height)*0.029*weight
 print('пройденная дистанция',dist/1000,'км,','сожженных калории',calor)
+
+if (dist/1000)<2:
+    print('Вы прошли слишком мало!Отдохните и постарайтель получше')
+elif ((dist/1000)>2 and (dist/1000)<4):
+    print('Среднячок,ты можешь лучше')
+else:
+    print('Молодец! Но не расслабляйся')
